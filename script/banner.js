579ae89fcd4fadadad3f29fd0aca7e6cf5219800
luckynie <luck_nie@163.com>
2018-12-10 22:36:31 +0800

    第四次提交

--- script/banner.js
+++ script/banner.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.index-bannerWrap .swiper-container', {
     loop: true,
     slidesPerView: 1,
     spaceBetween: 30,
@@ -23,3 +23,25 @@ $(".index-bannerWrap").mouseenter(function(){
 $(".index-bannerWrap").mouseleave(function(){
     swiper.autoplay.start();
 })
+for(let i=0;i<swiper.pagination.bullets.length;i++){
+    swiper.pagination.bullets[i].onmouseover=function(){
+        this.click();
+    };
+}
+    
+
+var swiper2 = new Swiper('.index-bannerWrap2 .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
--- script/index.js
+++ script/index.js
@@ -1,66 +1,56 @@
-// var swiper = new Swiper('.swiper-container', {
-//     loop: true,
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     effect: 'fade',
-//     autoplay: {
-//         delay: 2000,
-//         stopOnLastSlide: false,
-//         disableOnInteraction: true,
-//     },
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-// });
-// $(".index-bannerWrap").mouseenter(function(){
-//     swiper.autoplay.stop();
-// })
-// $(".index-bannerWrap").mouseleave(function(){
-//     swiper.autoplay.start();
-// })
-
 $.ajax({
     type:"get",
         url:"json/index.json",
         async:true,
         cache:true,
         success:function(res){ 
-            renderPage(res[0]);
+            console.log(res[0])
+renderPage(res[0]);
         }
 });
 function renderPage(json){ 
     //console.log(json)
     json.forEach(function(item,index){
-        console.log(item)
-        var $div1=$("<div>").addClass("tpl-recommed clearfix");
-        var str = ` <div class="floor-header clearfix">
-                        <h2 class="floor-title">${item.title}</h2>
-                        <span class="header-navs">MORE</span>
-                    </div>`
-        $div1.append(str);
-        var $div2 = $("<div>").addClass("tpl-body clearfix"); 
-        var $div3 = $("<div>").addClass("tpl-nav");
-        var $div4 = $("<div>").addClass("tpl-keywords");
-        var str1 ="";
-        for(var i=0;i<item.keywordsSrc.length;i++){
+        let str1="";
+        for(let i=0;i<item.keywordsSrc.length;i++){
             str1 += `<a class="keywords0" href=""><img class="lazy" src="${item.keywordsSrc[i]}"></a>`
         }
-        $div4.html(str1);
-        $div3.append($div4);
-       
-        var $div5 = $("<div>").addClass("tpl-category clearfix");
-        var str2=""
-        for(var j=0;j<item.label.length;i++){
-            str2 += `<a href="" title="${item.label[i]}">${item.label[i]}</a>`
+        let str2="";
+        for(var j=0;j<item.label.length;j++){
+                str2 += `<a href="" title="${item.label[j]}">${item.label[j]}</a>`
+             }
+        let str3="";
+        for(var k=0;k<item.list.length;k++){
+            str3 += `   <li>
+                            <a href=""><img class="lazy" src="${item.list[k]}"></a>
+                        </li>`;
         }
-        $div3.append($div5);
-        console.log($div1);
-        $div2.append($div3);
-        $("home-page").append($div1);
+        let str=`<div class="tpl-recommed clearfix">
+        <div class="floor-header clearfix" floorid="">
+            <h2 class="floor-title">${item.title}</h2>
+            <span class="header-navs">MORE</span>
+        </div>
+        <div class="tpl-body clearfix">
+            <div class="tpl-nav">
+                <div class="tpl-keywords">
+                   ${str1}
+                </div>
+                <div class="tpl-category clearfix">
+                   ${str2}
+                </div>
+            </div>
+            <div class="tpl-brands imgopacity clearfix">
+                <a title="卫衣" href=""><img class="lazy" " alt="潮流时装品牌" src="img/0116de8120c74eca1241e6cb059aba851f.jpg"></a>
+            </div>
+            <div class="tpl-types imgopacity clearfix">
+                <ul>
+                   ${str3}
+                </ul>
+            </div>
+        </div>
+    </div>`
+    
+    $(".home-page").append(str)
     })
 }
+
